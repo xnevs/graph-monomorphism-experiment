7d96c87a856f0965525eb0ce28ff100dc8d362f2
Sven Cerk <sven.cerk@gmail.com>
2018-08-19 23:10:19 +0200

    first scenarios

---
+++ scenarios/bt_order.sh
@@ -0,0 +1,1 @@
+python scripts/run.py --database=results.db ./progs/amalfi/sics/backtracking_degreeprune_ind_DEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind_RDEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind/a.out < test_cases/mivia/graphsdb/si2_rand_m200.sample
