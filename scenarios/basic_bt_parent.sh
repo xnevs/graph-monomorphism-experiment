982e052570e00d55f795f5cdcea1359f8765f220
Sven Cerk <sven.cerk@gmail.com>
2018-09-03 01:21:04 +0200

    MOAR!!!

---
+++ scenarios/basic_bt_parent.sh
@@ -0,0 +1,1 @@
+python scripts/run.py --database=results/graphsdb.db --timeout=20 ./progs/amalfi/sics/backtracking_parent_ind/a.out ./progs/amalfi/sics/backtracking_parent_adjacentconsistency_ind/a.out ./progs/amalfi/sics/backtracking_parent_adjacentconsistency_forwardcount_ind/a.out < test_cases/mivia/graphsdb/si_m400
