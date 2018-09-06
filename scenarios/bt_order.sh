b3fdf33a8075d18992c2777f50f04bdb039269ea
Sven Cerk <sven.cerk@gmail.com>
2018-09-06 23:00:27 +0200

    basic_order scenario

--- scenarios/bt_order.sh
+++ scenarios/bt_order.sh
@@ -1,1 +1,1 @@
-  python scripts/run.py --database=results/bt_order.db ./progs/amalfi/sics/backtracking_degreeprune_ind_DEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind_RDEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind/a.out < test_cases/mivia/graphsdb/si2_rand_m200.sample
+python scripts/run.py --database=results/bt_order.db ./progs/amalfi/sics/backtracking_degreeprune_ind_DEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind_RDEG/a.out ./progs/amalfi/sics/backtracking_degreeprune_ind/a.out < test_cases/mivia/graphsdb/si2_rand_m200.sample
